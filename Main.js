fb5d620b3526db062d9363e11de0be75590527be
Shoaib Muhammad <81280268+Shooaiib-Muhammad@users.noreply.github.com>
2021-06-01 15:32:38 +0500

    Java 2nd step

--- Main.js
+++ Main.js
@@ -1,3 +1,38 @@
-console.log('Heloo i am Log')
-console.error('Heloo i am Error')
-console.warn('Heloo i am Warning')
+// console.log('Heloo i am Log')
+// console.error('Heloo i am Error')
+// console.warn('Heloo i am Warning')
+// variables types Var, Let, const
+// let age = 30
+// console.log(age)
+// let Score
+// Score = 200
+// console.log(Score)
+// Data types String,Number,BooleanmNull,undefine,Symbol
+// const name = 'Shoaib Muhammad s/o Abdul Sittar'
+// const age = 30
+// const rating = 4.5
+// const iscool = true
+// const x = null
+// const y = undefined
+// let z
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof rating)
+// console.log(typeof iscool)
+// console.log(typeof c)
+// console.log(typeof y)
+// // Concatenation
+// console.log('My name is ' + name + ' and my age is ' + age)
+// // tempalte String
+// console.log(`my name is ${name} and my age is ${age}`)
+// Different properties of String
+const name = 'Shoaib Muhammad s/o Abdul Sittar'
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.substring(0, 15)) // Eloberate start and  end of String
+console.log(name.substring(0, 15).toUpperCase())
+console.log(name.substring(0, 15).toLowerCase())
+console.log(name.split('')) // Array COnversion
+const s = 'technolog,Cricket,playing,2020' // String into Array Formate
+console.log(s.split(', '))
